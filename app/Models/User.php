7f8d64fc0696ff07f9d68b869dd43a22b69b40d9
Maaz Khalid <khalidmaaz766@gmail.com>
2025-10-24 21:02:12 +0500

    fdfdfdf

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,8 +10,8 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable , Billable;
-;
+    use HasFactory, Notifiable ;
+
 
 
     /**
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Models\User;
-use Laravel\Cashier\Cashier;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +20,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Cashier::useCustomerModel(User::class);
     }
 }
