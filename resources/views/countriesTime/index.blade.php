475340ac159934de6291770107f6b951463be99d
Maaz Khalid <khalidmaaz766@gmail.com>
2025-11-11 00:24:14 +0500

    countries times changes

--- resources/views/countriesTime/index.blade.php
+++ resources/views/countriesTime/index.blade.php
@@ -13,19 +13,33 @@
         <p class="text-center mt-2 max-w-2xl mx-auto text-gray-700 dark:text-gray-300">
             Explore the current time in various countries around the world with our live time calculator. Instantly check and compare the local time of different nations, including major cities, and convert your local time to any country's timezone with ease.
 
+            <?php
+            $countries = [
+                [
+                    'name' => 'New York Current time',
+                    'summary' => 'Check the current time in New York City, USA, with our live time calculator. Stay updated with accurate local time and timezone information for New York.',
+                    'route' => 'newyork.time'
+                ],
+                [
+                    'name' => 'Australia Current time',
+                    'summary' => 'Get the current time in Australia with our live time calculator. View accurate local time and timezone details for cities across Australia, including Sydney, Melbourne, and Brisbane.',
+                    'route' => 'australia.time'
+                ],
+            ];
+
+            ?>
+
 
         <div class="border bg-red-100/30 border-1 border-red-300 my-8 rounded-lg p-4 shadow-card dark:bg-slate-800 dark:border-slate-700 dark:text-gray-200">
-            <div class="grid grid-cols-3 md:grid-cols-6 gap-3">
-                <a href="{{ route('newyork.time') }}" class="group rounded-xl border  border-red-300 bg-red-100 p-4 hover:shadow-card transition dark:bg-slate-900 dark:border-slate-700">
-                    <div class="font-semibold">New York</div>
-                    <div class="text-xs text-slate-500">New York current time</div>
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-3">
+                @foreach ($countries as $country)
+                <a href="{{ route($country['route']) }}" class="group rounded-xl border  border-red-300 bg-red-100 p-4 hover:shadow-card transition dark:bg-slate-900 dark:border-slate-700">
+                    <h1 class="font-semibold">{{ $country['name'] }}</h1>
+                    <div class="text-xs text-slate-500">{{ $country['summary'] }}</div>
                     <div class="mt-3 text-brand text-sm opacity-0 group-hover:opacity-100 transition">Open →</div>
                 </a>
-                <a href="{{ route('australia.time') }}" class="group rounded-xl border  border-red-300 bg-red-100 p-4 hover:shadow-card transition dark:bg-slate-900 dark:border-slate-700">
-                    <div class="font-semibold">Australia</div>
-                    <div class="text-xs text-slate-500">Australia</div>
-                    <div class="mt-3 text-brand text-sm opacity-0 group-hover:opacity-100 transition">Open →</div>
-                </a>
+                @endforeach
+
             </div>
         </div>
 
